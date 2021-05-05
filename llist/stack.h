a3130657876d0c9add1e440ececf529d4a5cfd4a
cnnrmnn <connor@cnnrmnn.com>
2021-05-05 10:07:40 -0400

    Switch to C-style comments

--- llist/stack.h
+++ llist/stack.h
@@ -1,16 +1,16 @@
 typedef struct stack *Stack;
 
-// Allocates and returns an empty stack
+/* Allocates and returns an empty stack */
 Stack s_alloc();
 
-// Frees the stack (should be empty)
+/* Frees the stack (should be empty) */
 void s_free(Stack s);
 
-// Pops and returns a value off of the stack
+/* Pops and returns a value off of the stack */
 int s_pop(Stack *s);
 
-// Pushes the given value onto the stack
+/* Pushes the given value onto the stack */
 void s_push(Stack *s, int val);
 
-// Returns true if the given stack is empty
+/* Returns true if the given stack is empty */
 int s_empty(const Stack *s);
